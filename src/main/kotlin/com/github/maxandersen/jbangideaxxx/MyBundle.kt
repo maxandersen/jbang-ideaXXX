57017a1f8967346d0b88aeffe8ebbcfb2f40aaea
GitHub Action <action@github.com>
2021-12-29 06:19:48 +0000

    Template cleanup

--- src/main/kotlin/com/github/maxandersen/jbangideaxxx/MyBundle.kt
+++ src/main/kotlin/com/github/maxandersen/jbangideaxxx/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.maxandersen.jbangideaxxx
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/maxandersen/jbangideaxxx/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/maxandersen/jbangideaxxx/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.maxandersen.jbangideaxxx.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.maxandersen.jbangideaxxx.MyBundle
 
 class MyApplicationService {
 
